be7a1527eaf849dd481f0aae0120f9af56c15007
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-24 05:36:47 +0000

    some important about datatype and variables concept in javascript

---
+++ 01_basics/2_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // this syntax is used in browser
+
+console.log ( 3 + 3)
+
+let name = "tanisha"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique
+// basically used when we deal with react uniqueness
+
+// getting the type of any
+
+console.log(typeof undefined)
+console.log(typeof(state))
+
+/* important thing is that type of undefined is undefined 
+ but type of null is object in javascript */
