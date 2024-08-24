0f69b9210c35820bac59e5d434dbd7c396b2da89
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-24 06:47:15 +0000

    some known facts about variables in js

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,25 @@
+// console.log("Hello welcome to the javascript file")
+
+// variables declaration
+
+const accountId = 144553
+let accountEmail = "220010043@iitdh.ac.in"
+
+var accountPassowrd = "12345"
+
+accountCity = "Jaipur"
+
+// accountId = 2  // not allowed
+let accountDetails;
+accountPassowrd = "34567"
+// both methods below works well
+console.table([accountId,accountEmail,accountPassowrd,accountCity,accountDetails]);
+console.log(accountDetails,accountId);
+
+/* few notes after first video
+    don't use var there is problem of block scope with it
+    use only let and const , once const its value cannot be changes
+    if any variable value is not declared then it is treated as undefined
+    semicoln can be used or not
+    print multiple variables in a tabele
+*/
